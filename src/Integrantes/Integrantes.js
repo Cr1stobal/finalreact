a5cd96b02d25f9d9b0c652d8bee3e6b787b82e84
405 <405@academicos.cesde.edu.co>
2022-12-13 11:16:08 -0500

    ok

--- src/Integrantes/Integrantes.js
+++ src/Integrantes/Integrantes.js
@@ -1,73 +1,68 @@
 import { Footer } from "../shared/Footer/Footer";
-export function Integrantes(){
-    
-        let titulo = "Integrantes";
+export function Integrantes() {
+  let titulo = "Integrantes";
 
-        let integrantes = [
-            {
-              nombre: "Brandon Boyd",
-              Foto: "https://firebasestorage.googleapis.com/v0/b/spotycaa.appspot.com/o/brandon.jpg?alt=media&token=1bfca369-59f4-4216-b432-33bbf6905280",
-              Biografia: "Nacimiento: 15 de febrero de 1976 (edad 46 años)",
-            },
-            {
-              nombre: "Mike Einziger",
-              foto: "https://firebasestorage.googleapis.com/v0/b/spotycaa.appspot.com/o/Mike2.jpg?alt=media&token=39e9edce-fd6d-4fb2-827c-367c3abb00b6",
-              Biografia: "Nacimiento: 21 de junio de 1976 (edad 46 años)",
-            },
-            {
-              nombre: "Alez Katunich",
-              foto: "https://firebasestorage.googleapis.com/v0/b/spotycaa.appspot.com/o/Alex4.jpg?alt=media&token=aa61dd8a-5cc9-48c5-a281-2887f2e89640",
-              Biografia: "Nacimiento: 18 de agosto de 1976 (edad 46 años)",
-            },
-            {
-              nombre: "Jose Pasillas",
-              foto: "https://firebasestorage.googleapis.com/v0/b/spotycaa.appspot.com/o/Jose5%2Cjpg.jpg?alt=media&token=467866fb-4b94-4ad4-b33a-409c3288dffb",
-              Biografia: "Nacimiento: 26 de abril de 1976 (edad 46 años)",
-            },
-            {
-              nombre: "Chris Kilmore",
-              foto: "https://firebasestorage.googleapis.com/v0/b/spotycaa.appspot.com/o/Chris1.jpg?alt=media&token=124e94a6-bad8-4bb9-abeb-215044984586",
-              Biografia: "Nacimiento: 21 de enero de 1973 (edad 49 años)",
-            },
-            {
-              nombre: "Ben Kenney",
-              foto: "https://firebasestorage.googleapis.com/v0/b/spotycaa.appspot.com/o/Ben3.jpg?alt=media&token=ba6b529f-3672-4ef1-929b-919f7e22dbd6",
-              Biografia: "Nacimiento: 12 de marzo de 1977 (edad 45 años)",
-            },
+  let integrantes = [
+    {
+      nombre: "Brandon Boyd",
+      foto: "https://firebasestorage.googleapis.com/v0/b/spotycaa.appspot.com/o/CoreyTaylor2.jpg?alt=media&token=4de33080-7a3d-4494-a701-10245ade0cf1",
+      Biografia: "Nacimiento: 15 de febrero de 1976 (edad 46 años)",
+    },
+    {
+      nombre: "Mike Einziger",
+      foto: "",
+      Biografia: "Nacimiento: 21 de junio de 1976 (edad 46 años)",
+    },
+    {
+      nombre: "Jay Weinberg",
+      foto: "",
+      Biografia: "Nacimiento: 18 de agosto de 1976 (edad 46 años)",
+    },
+    {
+      nombre: "Jose Pasillas",
+      foto: "",
+      Biografia:""
+    },
+    {
+      nombre: "Chris Kilmore",
+      foto: "",
+      Biografia: "Nacimiento: 21 de enero de 1973 (edad 49 años)",
+    },
+    {
+      nombre: "Ben Kenney",
+      foto: "",
+      Biografia: "Nacimiento: 12 de marzo de 1977 (edad 45 años)",
+    },
+  ];
 
-        ];
-
-        return (
-            <>
-              <h1>{titulo}</h1>
-              <div className="container">
-                <div className="row row-cols-1 row-cols-md-3 g-5">
-                  {integrantes.map(function (integrantes) {
-                    return (
-                      <>
-                        <div className="col">
-                          <div className="card h-100 shadow">
-                            <img
-                              src={integrantes.foto}
-                              alt="foto"
-                              className="h-100 img fluid w-100"
-                            />
-                            <h2 className="text-center">{integrantes.nombre}</h2>
-                            <p className="text-center">Precio: {integrantes.Biografia
-                            }$</p>
-                          </div>
-                        </div>
-                      </>
-                    );
-                  })}
+  return (
+    <>
+      <h1>{titulo}</h1>
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-3 g-5">
+          {integrantes.map(function (integrante) {
+            return (
+              <>
+                <div className="col">
+                  <div className="card h-100 shadow">
+                   
+                    <img
+                      src={integrante.foto}
+                      alt="foto"
+                      className="h-100 img fluid w-100"
+                    />
+                    <h2 className="text-center">{integrante.nombre}</h2>
+                    <p className="text-center">
+                      Precio: {integrante.Biografia}$
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <Footer />
-            </>
-          );
-
-            
-            
-            
-      
+              </>
+            );
+          })}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
